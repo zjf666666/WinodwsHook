85bd4779d0bd67f6085883137462f312292de907
zhengjunfeng <1501335608@qq.com>
2025-09-10 19:23:07 +0800

    提交gtest相关配置及线程注入测试用例

--- WindowsSecurityGuard/SecurityTests/CreateRemoteThreadTest.cpp
+++ WindowsSecurityGuard/SecurityTests/CreateRemoteThreadTest.cpp
@@ -4,6 +4,6 @@
 
 TEST(CreateRemoteThreadStrategy, Inject) {
     CreateRemoteThreadStrategy remotethread;
-    bool bRes = remotethread.Inject(1, L"23");
+    bool bRes = remotethread.Inject(21060, L"C:\\Users\\15013\\Desktop\\MyInlineHook.dll");
     EXPECT_EQ(bRes, true);
 }
